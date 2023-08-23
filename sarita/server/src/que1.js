2f6c6bb9a6d2f2fbc0c58543866abab88afd3e31
onyournn <saritam64@nu.ac.th>
2023-08-23 12:35:59 +0700

    page2

---
+++ sarita/server/src/que1.js
@@ -0,0 +1,17 @@
+(function (a, b) {
+    return a + b + 100;
+  });
+  
+  // Arrow function
+  (a, b) => a + b + 100;
+  
+  const a = 4;
+  const b = 2;
+  
+  // Traditional anonymous function (no parameters)
+  (function () {
+    return a + b + 100;
+  });
+  
+  // Arrow function (no parameters)
+  () => a + b + 100;
