0c7acc641470cdcda39b00a3b42a84a1dd62f481
onyournn <saritam64@nu.ac.th>
2023-08-23 13:50:41 +0700

    page3

--- sarita/server/src/que1.js
+++ sarita/server/src/que1.js
@@ -1,7 +1,7 @@
 let express = require('express')
 const que1 = express()
 
-que1.set('/status', function(req,res){
+que1.get('/que1', function(req,res){
     res.send(a+b)
 })
 
@@ -10,3 +10,6 @@ let port = 8081
 que1.listen(port,function (){
     console.log(port)
 })
+//(function (a, b) {
+    ///return a + b + 100;
+//});
