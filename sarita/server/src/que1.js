fb53d0b8882545f5737a297adfbf6d2b52730921
onyournn <saritam64@nu.ac.th>
2023-08-23 13:41:54 +0700

    page2

--- sarita/server/src/que1.js
+++ sarita/server/src/que1.js
@@ -1,6 +1,12 @@
-function que1(){
-    console.log(a+b);
-    
-    }
-    
-    module.exports = ''
+let express = require('express')
+const que1 = express()
+
+que1.set('/status', function(req,res){
+    res.send(a+b)
+})
+
+let port = 8081
+
+que1.listen(port,function (){
+    console.log(port)
+})
