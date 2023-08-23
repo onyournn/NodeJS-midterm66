bc6547756f8133eb9554e58baf675a0761cec003
onyournn <saritam64@nu.ac.th>
2023-08-23 13:11:20 +0700

    page2

--- sarita/server/src/que1.js
+++ sarita/server/src/que1.js
@@ -1,17 +1,3 @@
-let express = require('express')
-let bodyParser = require('body-parser')
-
-const {sequelize} = require('./models')
-const config = require('./config/config')
-
-
-const que1 = express()
-
-que1.use(bodyParser.json())
-que1.use(bodyParser.urlencoded({ extended: true }))
-
-require('./routes')(que1)
-
-que1.get('/status', function (req, res) {
-    res.send('')
-})
+(function (a, b) {
+    return a + b ;
+  });
